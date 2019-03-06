c39c279370c920cf6dc08c53601c22f90e9c351c
SMIMA <youssef.x.smima.-nd@disney.com>
2019-03-06 09:52:41 +0100

    [evol] adding unit testing for dao layer

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Adservio on 07/12/2018.
@@ -15,13 +17,33 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "User")
 @Table(name = "USER")
-public class User implements Serializable{
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "VERSION")
     private String version;
+
+    @Column(name = "CREATE_DATE")
     private LocalDate creationDate;
+
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+
+    @Column(name = "LASTNAME")
+    private String lastname;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @ManyToMany
+    private List<Role> roles = new ArrayList<>();
+
 }
