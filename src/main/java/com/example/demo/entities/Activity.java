aa60d844761fdb93d936e8c047dfd631e6b6bba9
yawyaw <youssef.smimaa@gmail.com>
2019-03-11 15:16:58 +0100

    test

--- src/main/java/com/example/demo/entities/Activity.java
+++ src/main/java/com/example/demo/entities/Activity.java
@@ -3,13 +3,11 @@ package com.example.demo.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity(name = "Activity")
 @Table(name = "ACTIVITY")
 public class Activity {
@@ -17,4 +15,8 @@ public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    public Activity(){
+
+    }
 }
